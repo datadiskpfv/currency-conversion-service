49c6a2b4b0b04bcb6045297cd4748e65cc35be23
pvalle <paul.valle@orbian.com>
2019-01-23 13:51:34 +0000

    initial commit

--- src/main/java/uk/co/datadisk/microservices/currencyconversionservice/controllers/CurrencyConversionController.java
+++ src/main/java/uk/co/datadisk/microservices/currencyconversionservice/controllers/CurrencyConversionController.java
@@ -1,11 +1,13 @@
 package uk.co.datadisk.microservices.currencyconversionservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import uk.co.datadisk.microservices.currencyconversionservice.entities.CurrencyConversionBean;
+import uk.co.datadisk.microservices.currencyconversionservice.proxies.CurrencyExchangeServiceProxy;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
@@ -13,6 +15,9 @@ import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
 
     @GetMapping("/currency-converter/from/{fromCurrency}/to/{toCurrency}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable String fromCurrency,
@@ -31,9 +36,30 @@ public class CurrencyConversionController {
 
         CurrencyConversionBean response = responseEntity.getBody();
 
+        // Using Feign to solve the complex code ssee method below
+
         System.out.println(response.getTotalCalculatedAmount());
         System.out.println(response.getId());
         System.out.println(response.getFromCurrency());
+
+        return new CurrencyConversionBean(
+                response.getId(),
+                fromCurrency,
+                toCurrency,
+                response.getConversionMultiple(),
+                quantity,
+                quantity.multiply(response.getConversionMultiple()),
+                response.getPort());
+    }
+
+    // Using Feign to solve the complex code above
+    @GetMapping("/currency-converter-feign/from/{fromCurrency}/to/{toCurrency}/quantity/{quantity}")
+    public CurrencyConversionBean convertCurrencyFeign(@PathVariable String fromCurrency,
+                                                  @PathVariable String toCurrency,
+                                                  @PathVariable BigDecimal quantity) {
+
+        // Use the proxy to get data from the currency exchange service
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(fromCurrency, toCurrency);
 
         return new CurrencyConversionBean(
                 response.getId(),
---
+++ src/main/java/uk/co/datadisk/microservices/currencyconversionservice/proxies/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,22 @@
+package uk.co.datadisk.microservices.currencyconversionservice.proxies;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import uk.co.datadisk.microservices.currencyconversionservice.entities.CurrencyConversionBean;
+
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
+public interface CurrencyExchangeServiceProxy {
+
+    @GetMapping("/currency-exchange/from/{fromCurrency}/to/{toCurrency}")
+    CurrencyConversionBean retrieveExchangeValue(@PathVariable String fromCurrency,
+                                                        @PathVariable String toCurrency);
+
+    //old way
+//    public CurrencyConversionBean retrieveExchangeValue(@PathVariable("fromCurrency") String fromCurrency,
+//                                                        @PathVariable("toCurrency") String toCurrency);
+
+}
